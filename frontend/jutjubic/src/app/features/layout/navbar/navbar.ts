621bcd6b15af0579293ed9e030a5a708140cc724
mareenah <kicanovicmarina@gmail.com>
2026-01-10 15:04:20 +0100

    feat: add dropdown menu in navbar

--- frontend/jutjubic/src/app/features/layout/navbar/navbar.ts
+++ frontend/jutjubic/src/app/features/layout/navbar/navbar.ts
@@ -1,11 +1,10 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { filter, map, startWith } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { User } from '../../../models/user.model';
 import { AuthService } from '../../../auth/auth.service';
-import { TokenStorage } from '../../../auth/jwt/token.service';
+import { User } from '../../../models/user.model';
 
 @Component({
   standalone: true,
@@ -15,14 +14,25 @@ import { TokenStorage } from '../../../auth/jwt/token.service';
   styleUrl: './navbar.css',
 })
 export class Navbar implements OnInit {
+  user: User | undefined;
   isLoginPage = false;
   isRegisterPage = false;
   loggedIn$: any;
   isAuthPage$: any;
+  dropdownOpen = false;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private elementRef: ElementRef
+  ) {}
 
   ngOnInit(): void {
+    this.authService.user$.subscribe((user) => {
+      this.user = user;
+      console.log('Navbar\n' + 'username: ' + user.username + ' id: ' + user.id);
+    });
+
     this.loggedIn$ = this.authService.loggedIn$;
 
     this.isAuthPage$ = this.router.events.pipe(
@@ -48,5 +58,22 @@ export class Navbar implements OnInit {
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
+    this.dropdownOpen = false;
+  }
+
+  toggleMenu() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (!this.elementRef.nativeElement.contains(target)) {
+      this.dropdownOpen = false;
+    }
+  }
+
+  home(): void {
+    this.router.navigate(['/']);
   }
 }
