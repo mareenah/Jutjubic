d18c7e2abad90f9c65beaa1934c7e9b147a7171f
mareenah <kicanovicmarina@gmail.com>
2026-02-11 22:17:06 +0100

    refactor: change apiHost and socketHost

--- frontend/jutjubic/src/environment/environment.ts
+++ frontend/jutjubic/src/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiHost: 'http://localhost:8080/api/',
+  apiHost: 'http://192.168.1.12:8080/api/',
+  socketHost: 'http://192.168.1.12:8080/',
 };
