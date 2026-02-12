732e70bf7b4ff9521835e37b0da31f64eb07a8fa
mareenah <kicanovicmarina@gmail.com>
2026-02-12 01:32:11 +0100

    feat: add watch party removal after its closure

--- backend/isa/src/main/java/com/example/jutjubic/contorller/WatchPartySocketController.java
+++ backend/isa/src/main/java/com/example/jutjubic/contorller/WatchPartySocketController.java
@@ -1,22 +1,35 @@
 package com.example.jutjubic.contorller;
 
 import com.example.jutjubic.dto.WatchPartyEventDto;
+import com.example.jutjubic.model.WatchParty;
+import com.example.jutjubic.service.WatchPartyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+
+import java.nio.file.AccessDeniedException;
+import java.util.UUID;
 
 @Controller
 public class WatchPartySocketController {
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
+    @Autowired
+    private WatchPartyService watchPartyService;
 
     @MessageMapping("/watch-party/start")
-    public void startVideo(WatchPartyEventDto event) {
+    public void startVideo(WatchPartyEventDto event) throws AccessDeniedException {
+        WatchParty party = watchPartyService.findWatchPartyById(event.getPartyId());
+
         simpMessagingTemplate.convertAndSend(
                 "/socket-publisher/watch-party/" + event.getPartyId(),
                 event
         );
+
+        watchPartyService.delete(party);
     }
 }
